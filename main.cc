a41afcc8871667aa4539f3ace8dc6f39335f02c0
Vincent Barrielle <vincent.barrielle@m4x.org>
2012-12-13 20:32:37 +0100

    main without logs

--- main.cc
+++ main.cc
@@ -10,11 +10,7 @@ static const int kDone = 2;
 int
 main( int argc, char ** argv )
 {
-#if 0
-  channel<int> sayHello, sayWorld, quitter;
-#else
   channel<int> sayHello(1), sayWorld(1), quitter(1);
-#endif
 
   std::cerr << "sayHello: " << &sayHello << "\n";
   std::cerr << "sayWorld: " << &sayWorld << "\n";
@@ -25,13 +21,9 @@ main( int argc, char ** argv )
         for ( int i = 0; i < 1000; ++i )
         {
           std::cout << "Hello ";
-          fprintf( stderr, "%s to %p\n", "Hello: say go", &sayWorld );
           sayWorld << kGo;
-          fprintf( stderr, "%s to %p\n", "Hello: said go", &sayWorld );
           int a;
-          fprintf( stderr, "%s from %p\n", "Hello: receive go", &sayHello );
           sayHello >> a;
-          fprintf( stderr, "%s from %p\n", "Hello: received go", &sayHello );
         }
         sayWorld << kQuit;
       } );
@@ -41,15 +33,11 @@ main( int argc, char ** argv )
         while ( true )
         {
           int reply;
-          fprintf( stderr, "%s from %p\n", "World: receive go", &sayWorld );
           sayWorld >> reply;
-          fprintf( stderr, "%s from %p\n", "World: received go", &sayWorld );
           if ( reply == kQuit )
             break;
           std::cout << "world!\n";
-          fprintf( stderr, "%s to %p\n", "World: say go", &sayHello );
           sayHello << kGo;
-          fprintf( stderr, "%s to %p\n", "World: said go", &sayHello );
         }
         quitter << kDone;
       } );
