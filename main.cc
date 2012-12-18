55c47178b31378f8ec05298ff2a3c68f42062354
Vincent Barrielle <vincent.barrielle@m4x.org>
2012-12-18 15:45:25 +0100

    added MIT license

--- main.cc
+++ main.cc
@@ -1,3 +1,25 @@
+/*
+* Copyright (c) 2012 Vincent Barrielle
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+* of the Software, and to permit persons to whom the Software is furnished to do
+* so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
 #include <iostream>
 #include <string>
 #include <future>
@@ -9,13 +31,9 @@ static const int kQuit = 1;
 static const int kDone = 2;
 
 int
-main( int argc, char ** argv )
+main( )
 {
-  channel<int> sayHello(0), sayWorld(0), quitter(0);
-
-  std::cerr << "sayHello: " << &sayHello << "\n";
-  std::cerr << "sayWorld: " << &sayWorld << "\n";
-  std::cerr << "quitter: " << &quitter << "\n";
+  channel<int> sayHello, sayWorld, quitter;
 
   auto d = std::async( std::launch::async, [&]
       {
@@ -45,20 +63,5 @@ main( int argc, char ** argv )
 
   int a;
   quitter >> a;
-
-  // test channel of capacity > 1
-
-  channel<int> chan( 6 );
-  for ( int i = 0; i < 6; ++i )
-    chan << i;
-
-  for ( int i = 0; i < 6; ++i )
-  {
-    int k;
-    chan >> k;
-    std::cerr << k << " ";
-  }
-  std::cerr << "\n";
-    
   return 0;
 }
