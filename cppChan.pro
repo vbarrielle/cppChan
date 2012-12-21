5304478489fc3f359c7b0c9cff75d6da19204c34
Vincent Barrielle <vincent.barrielle@m4x.org>
2012-12-21 11:19:36 +0100

    working tests

--- cppChan.pro
+++ cppChan.pro
@@ -1,11 +1,14 @@
 TEMPLATE = app
-TARGET = hello
+TARGET = channelTest
 DEPENDPATH += .
 QMAKE_CXXFLAGS += -g -Wall -Wextra -std=c++11 -O0
+LIBS += -lgtest
+QT -= gui
+QT -= core
 
 # Input
 HEADERS += channel.hh
-SOURCES += main.cc
+SOURCES += tests/test_channel.cc
 
 
 
--- tests/test_channel.cc
+++ tests/test_channel.cc
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include <future>
+#include "../channel.hh"
 
 
 TEST( test_channel, preserve_order )
@@ -14,53 +15,56 @@ TEST( test_channel, preserve_order )
       int res;
       chan >> res;
       ASSERT_EQ( res, j ) << "channel of capacity "
-        << i << " does not preserve order";
+         << i << " does not preserve order";
     }
   }
 }
 
 TEST( test_channel, blocks_when_full )
 {
-  for ( int i = 0; i < 100; ++i )
+  int lastLoop = 0;
+  static const int loopCount = 100;
+  for ( int i = 0; i < loopCount; ++i )
   {
     channel<int> chan( i );
     auto a = std::async( std::launch::async, [&]
         {
-          for ( int j = 0; j < i; ++j )
+          for ( int j = 0; j < i+1; ++j )
           {
             chan << j;
           }
           return 0;
         } );
-    auto b = std::async( std::launch::async, [&]
-        {
-          for ( int j = 0; j < i-1; ++j )
-          {
-            int res;
-            chan >> res;
-          }
-        } );
     auto status = a.wait_for( std::chrono::milliseconds( 10 ) );
-    ASSERT_EQ( status, std::future_status::timeout )
-      << "The channel is supposed to be full and to block";
+    bool ok = status != std::future_status::ready;
+
+    // now unlock the blocked thread (done before checking the result,
+    // to enable correct destruction of a if the test fails
+    int res;
+    for ( int j = 0; j < i+1; ++j )
+      chan >> res;
+    auto get = a.get( );
+
+    if ( !ok )
+      break;
+    lastLoop = i;
   }
+  ASSERT_TRUE( lastLoop == loopCount-1 ) << "The channel of size " << lastLoop
+    << " is supposed to be full and to block";
 }
 
 TEST( test_channel, blocks_when_empty )
 {
+  static const int loopCount = 100;
+  int lastLoop = 0;
   for ( int i = 0; i < 100; ++i )
   {
     channel<int> chan( i );
-    auto a = std::async( std::launch::async, [&]
-        {
-          for ( int j = 0; j < i-1; ++j )
-          {
-            chan << j;
-          }
-        } );
+    for ( int j = 0; j < i; ++j )
+      chan << j;
     auto b = std::async( std::launch::async, [&]
         {
-          for ( int j = 0; j < i; ++j )
+          for ( int j = 0; j < i+1; ++j )
           {
             int res;
             chan >> res;
@@ -68,7 +72,23 @@ TEST( test_channel, blocks_when_empty )
           return 0;
         } );
     auto status = b.wait_for( std::chrono::milliseconds( 10 ) );
-    ASSERT_EQ( status, std::future_status::timeout )
-      << "The channel is supposed to be empty and to block";
+    bool ok = status == std::future_status::timeout;
+
+    // unblock thread
+    chan << 0;
+    auto get = b.get( );
+
+    if ( !ok )
+      break;
+    lastLoop = i;
   }
+  ASSERT_TRUE( lastLoop == loopCount-1 ) << "The channel of size " << lastLoop
+    << " is supposed to be empty and to block";
+}
+
+int
+main( int argc, char ** argv )
+{
+  ::testing::InitGoogleTest( &argc, argv );
+  return RUN_ALL_TESTS( );
 }
